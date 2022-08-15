b0a2b28b960771ee73a5a0e9e8238d13fa482fd9
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-14 20:48:37 -0700

    commit tree 783248e57578bc6e233d130802a543042d360985

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
 
-// Write your code here
+let random = Math.ceil (Math.random()*10);
+
+let num3 = 22;
+let num4 = 6;
+let mod = num3%num4;
+
+let max = Math.max (17,20,3,16,19,5,9,14);
